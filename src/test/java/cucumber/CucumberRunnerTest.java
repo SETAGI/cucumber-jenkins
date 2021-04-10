6bf15afd5b20c7755ff2c33f0f14e6bc0989b71e
Sebastian Tamayo Giraldo <sebastian.tamayo@globant.com>
2021-04-10 15:48:34 -0500

    Adding an instruction for future jobs

--- src/test/java/cucumber/CucumberRunnerTest.java
+++ src/test/java/cucumber/CucumberRunnerTest.java
@@ -4,6 +4,8 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 import org.junit.runner.RunWith;
 
+//This code has a integration with jenkins and gitHub, it runs a job when it detects a change in the code.
+
 @RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/java/features/mavenTest.feature",
